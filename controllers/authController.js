a0edb8edb78af3c5fe6cd6090bed4fd99f71995c
Vũ Trọng Đạt <81927305+datvuhp2002@users.noreply.github.com>
2024-12-11 12:06:55 +0700

    config middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,9 @@
-import * as authService from '../services/authService.js';
+import * as authService from "../services/authService.js";
 
 const login = async (req, res) => {
+  console.log(req.body);
   try {
     const { email, password } = req.body;
-
     const result = await authService.login(email, password);
     if (!result.success) {
       return res.status(result.status).json({ message: result.message });
@@ -11,7 +11,7 @@ const login = async (req, res) => {
 
     res.json(result.data);
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: "Internal server error", error });
   }
 };
 
@@ -26,7 +26,7 @@ const register = async (req, res) => {
 
     res.status(201).json(result.data);
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: "Internal server error", error });
   }
 };
 
@@ -41,7 +41,7 @@ const refreshToken = async (req, res) => {
 
     res.status(200).json(result.data);
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: "Internal server error", error });
   }
 };
 
