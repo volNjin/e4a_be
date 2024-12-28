123ec709d3f990ca80b4bd76ff408a3a6d02020d
Vũ Trọng Đạt <81927305+datvuhp2002@users.noreply.github.com>
2024-12-28 17:19:22 +0700

    add authentication

--- routes/ckEditorRoute.js
+++ routes/ckEditorRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import upload from "../config/multer.js";
+import authenticate from "../middlewares/authMiddleware.js";
 
 const ckRouter = express.Router();
+ckRouter.use(authenticate);
 
 ckRouter.post("/", (req, res) => {
   const uploadMiddleware = upload.single("upload"); // Tên field phải trùng với tên field trong FormData
   uploadMiddleware(req, res, (err) => {
-    console.log(req.body);
     if (err) {
       console.error("Upload error:", err.message); // Log chi tiết lỗi
       return res.status(400).json({
@@ -17,14 +18,12 @@ ckRouter.post("/", (req, res) => {
     if (!req.file) {
       console.error("No file uploaded");
       return res.status(400).json({
-        error: "No file uploaded", // Lỗi khi không có file
+        error: "No file uploaded",
       });
     }
 
-    // Thành công
-    console.log("File uploaded:", req.file);
     res.status(200).json({
-      url: `/ck-editor/${req.file.filename}`, // Đường dẫn file đã upload
+      url: `/ck-editor/${req.file.filename}`,
     });
   });
 });
